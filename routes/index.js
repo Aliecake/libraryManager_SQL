df48690ac747eccce9f37214c3e774f4ddcdefd3
Lauren Eggers <neyabenz@gmail.com>
2020-03-02 17:31:04 -0500

    test, connections

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,52 @@
 const express = require(`express`);
+const db = require('../db/index')
+
+const { Book } = db.models;
 
 const router = express.Router();
 
+//TEST DB connection
+(async () => {
+    try {
+        await db.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+});
+
+
 
 router.get(`/`, (req, res) => {
-    res.render(`index`)
+    
+    (async () => {
+        // await db.sequelize.sync({alter: true})
+        try {
+            const books = await Book.findAll({
+                order: [[`id`, `ASC`]]
+            })
+            res.render(`index`, { books })
+        } catch(err) {
+            console.log(err)
+        }
+        
+    })()
+
 });
 
 router.get(`/book/:id`, (req, res) => {
-    res.send(`book ${req.params.id}`)
+    (async () => {
+        await db.sequelize.sync({alter: true})
+        try {
+            const book = await Book.findByPk(req.params.id)
+            console.log(book)
+            res.render(`book_detail`, { book : book.dataValues })
+        } catch(err) {
+            console.log(err)
+        }
+        
+    })()
+ 
 });
 
 router.get(`/book/:id/details`, (req, res) => {
