db253353ad4f972eb47cbc6125d02ee7321cf46e
Lauren Eggers <neyabenz@gmail.com>
2020-03-02 17:29:36 -0500

    db connect, get data

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ const path = require(`path`);
 const mainRoutes = require(`./routes/index`)
 const errorRoutes = require(`./routes/errors`)
 
+
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
-app.use(`/static`, express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 
-app.use(mainRoutes)
-app.use(errorRoutes)
+app.use(mainRoutes);
+app.use(errorRoutes);
 
-app.set(`view engine`, `pug`)
+app.set(`view engine`, `pug`);
 
 
 app.listen(port, () => {
-    console.log(`Weeeeee, I'm listening on port 3001`)
+    console.log(`Weeeeee, I'm listening on port 3000`);
 });
 
 module.exports = app;
