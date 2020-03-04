7c4b2823026d4970866d2e0ecc7401d258f2d02c
Lauren Eggers <neyabenz@gmail.com>
2020-03-04 14:27:58 -0500

    cleaned up async, middleware

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,32 @@
-const express = require(`express`);
-const db = require('../db/index')
+const express = require(`express`),
+    db = require('../db/index'),
+    middleware = require(`../middleware`);
 
 const { Book } = db.models;
 
 const router = express.Router();
 
 //-- INDEX --//
-router.get(`/`, async (req, res) => {
+router.get(`/`, middleware.asyncHandler(async (req, res) => {
     await db.sequelize.sync({alter: true})
-        try {
 
-            const books = await Book.findAll({
-                order: [[`id`, `ASC`]]
-            })
-            res.render(`index`, { books })
-        } catch(err) {
-            res.render(`error`, { err })
-        }
-
-});
+        const books = await Book.findAll({
+            order: [[`id`, `ASC`]]
+        })
+        res.render(`index`, { books })
+}));
 
 //-- CREATE ROUTE --//
 router.get(`/books/new_book`, (req, res) => {
     res.render(`new`)
-})
-
-//-- DETAIL ROUTE --//
-router.get(`/books/:id/details`, async (req, res) => {
-
-        try {
-            const book = await Book.findByPk(req.params.id)
-
-            res.render(`book_detail`, { book : book.dataValues })
-
-        } catch(err) {
-            res.render(`error`, { err })
-        }
-
 });
 
+//-- DETAIL ROUTE --//
+router.get(`/books/:id/details`, middleware.asyncHandler(async (req, res) => {
+    const book = await Book.findByPk(req.params.id)
+
+    res.render(`book_detail`, { book : book.dataValues })
+}));
 
 
 module.exports = router;
